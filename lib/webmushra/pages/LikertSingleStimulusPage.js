piano-eval/piano-eval.github.io

Allow adding questionnaires to LikertSingleStimulusPages, replacing Likert scales for more general questionnaires

lib/webmushra/pages/LikertSingleStimulusPage.js
@@ -5,70 +5,44 @@ This source code is protected by copyright law and international treaties. This 
 
 **************************************************************************/
 
-function LikertSingleStimulusPage(_pageManager, _pageTemplateRenderer, _pageConfig, _audioContext, _bufferSize, _audioFileLoader, _stimulus, _session, _errorHandler, _language) {
-  this.pageManager = _pageManager;
-  this.pageTemplateRenderer = _pageTemplateRenderer; 
-  this.pageConfig = _pageConfig;  
+LikertSingleStimulusPage.prototype = Object.create(Page.prototype);
+LikertSingleStimulusPage.prototype.constructor = LikertSingleStimulusPage;
+
+function LikertSingleStimulusPage(_pageManager, _pageTemplateRenderer, _pageConfig, _session, _language, _audioContext, _bufferSize, _audioFileLoader, _errorHandler, _stimulus) {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).constructor.call(this, _pageManager, _pageTemplateRenderer, _pageConfig, _session, _language);
   this.audioContext = _audioContext;
   this.bufferSize = _bufferSize;
   this.audioFileLoader = _audioFileLoader;
-  this.stimulus = _stimulus;  
-  this.session = _session;      
   this.errorHandler = _errorHandler;
-  this.language = _language;
-  this.fpc = null;    
+  this.stimulus = _stimulus;
+  this.fpc = null;
     
   this.audioFileLoader.addFile(this.stimulus.getFilepath(), (function (_buffer, _stimulus) { _stimulus.setAudioBuffer(_buffer); }), this.stimulus);
   this.filePlayer = null;
   this.waveformVisualizer = null;
-  this.likerts = [];
   this.ratingMap = {};
     
-  this.time = 0; 
+  this.time = 0;
   this.startTimeOnPage = null;
-  this.results= [];
 } 
 
-LikertSingleStimulusPage.prototype.getName = function () {
-  return this.pageConfig.name;
-};
-
-LikertSingleStimulusPage.prototype.init = function (_callbackError) { 
+LikertSingleStimulusPage.prototype.init = function (_callbackError) {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).init.call(this);
   this.filePlayer = new FilePlayer(this.audioContext, this.bufferSize, [this.stimulus], this.errorHandler, this.language, this.pageManager.getLocalizer(), this.pageConfig.showWaveform);
   
-  var cbk = (function(_prefix) {
-    this.ratingMap[_prefix] = true;
-    if (Object.keys(this.ratingMap).length == this.likerts.length) {
-      this.pageTemplateRenderer.unlockNextButton();
-    }
-  }).bind(this);
-  
-  
-  if (this.pageConfig.mustRate === false) {
-    cbk = false;  
-  }
-  
-  if (Array.isArray(this.pageConfig.response[0])) {
-    for (var i = 0; i < this.pageConfig.response.length; ++i) {
-      this.likerts.push(new LikertScale(this.pageConfig.response[i], i + '_', this.pageConfig.mustPlayback, cbk));
-    }
-  } else {
-    this.likerts.push(new LikertScale(this.pageConfig.response, '1_', this.pageConfig.mustPlayback, cbk));
-  }
- 
   if (this.pageConfig.mustPlayback) {
     this.filePlayer.genericAudioControl.addEventListener((function (_event) {
       if (_event.name == this.pageConfig.mustPlayback) {
-        this.likerts.map(function(l) {l.enable();});
+        for (i = 0; i < this.pageConfig.questionnaire.length; ++i) {
+          var element = this.pageConfig.questionnaire[i];
+          if (element.type === "likert") {
+            $('input[name='+element.name+'_response]').checkboxradio('enable');
+          }
+        }
       } 
     }).bind(this));
-
   }
-
 };
-  
-
-
 
 LikertSingleStimulusPage.prototype.render = function (_parent) {  
   var div = $("<div></div>");
@@ -93,42 +67,28 @@ LikertSingleStimulusPage.prototype.render = function (_parent) {
     this.waveformVisualizer.load();
   }
   this.filePlayer.render(_parent);
-
   
-  this.likerts.map(function(l) {l.render(_parent);});
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).render.call(this, _parent);
   
   this.fpc = new FilePlayerController(this.filePlayer);
   this.fpc.bind();
 };
 
-LikertSingleStimulusPage.prototype.load = function () {  
+LikertSingleStimulusPage.prototype.load = function () {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).load.call(this);
   this.startTimeOnPage = new Date();
-  if(this.pageConfig.mustRate == true){
-    this.pageTemplateRenderer.lockNextButton();
-  }
-  if(this.results.length > 0){
-    for (var i = 0; i < this.likerts.length; ++i) {
-      $("input[name='"+this.likerts[i].prefix +"_response'][value='"+this.results[i]+"']").attr("checked", "checked");
-      $("input[name='"+this.likerts[i].prefix +"_response'][value='"+this.results[i]+"']").checkboxradio("refresh");
-      this.likerts[i].group.change();
-    }
-  }
-  
   this.filePlayer.init();
 };
 
 LikertSingleStimulusPage.prototype.save = function () {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).save.call(this);
   this.fpc.unbind(); 
   this.time += (new Date() - this.startTimeOnPage);
-  
-  for (var i = 0; i < this.likerts.length; ++i) {
-    this.results[i] = $("input[name='"+this.likerts[i].prefix +"_response']:checked").val();
-  }
   
   this.filePlayer.free();
 };
 
-LikertSingleStimulusPage.prototype.store = function (_reponsesStorage) {
+LikertSingleStimulusPage.prototype.store = function () {
   var trial = this.session.getTrial(this.pageConfig.type, this.pageConfig.id);
   if (trial === null) {
     trial = new Trial();
@@ -139,17 +99,8 @@ LikertSingleStimulusPage.prototype.store = function (_reponsesStorage) {
   var rating = new LikertResponse();
   rating.stimulus = this.stimulus.id;
   
-  rating.stimulusRating = [];
-  for(var i = 0; i < this.likerts.length; ++i){
-    if(this.results[i] === undefined){
-      rating.stimulusRating.push("NA");
-    }else{
-      rating.stimulusRating.push(this.results[i]);
-    }
-  }
-      
+  rating.stimulusRating = {};
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).store.call(this, rating.stimulusRating);
   rating.time = this.time;
   trial.responses[trial.responses.length] = rating;
-
-
 };

nkundiushuti/webMUSHRA_jp

lib/webmushra/pages/LikertSingleStimulusPage.js
@@ -5,70 +5,44 @@ This source code is protected by copyright law and international treaties. This 
 
 **************************************************************************/
 
-function LikertSingleStimulusPage(_pageManager, _pageTemplateRenderer, _pageConfig, _audioContext, _bufferSize, _audioFileLoader, _stimulus, _session, _errorHandler, _language) {
-  this.pageManager = _pageManager;
-  this.pageTemplateRenderer = _pageTemplateRenderer; 
-  this.pageConfig = _pageConfig;  
+LikertSingleStimulusPage.prototype = Object.create(Page.prototype);
+LikertSingleStimulusPage.prototype.constructor = LikertSingleStimulusPage;
+
+function LikertSingleStimulusPage(_pageManager, _pageTemplateRenderer, _pageConfig, _session, _language, _audioContext, _bufferSize, _audioFileLoader, _errorHandler, _stimulus) {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).constructor.call(this, _pageManager, _pageTemplateRenderer, _pageConfig, _session, _language);
   this.audioContext = _audioContext;
   this.bufferSize = _bufferSize;
   this.audioFileLoader = _audioFileLoader;
-  this.stimulus = _stimulus;  
-  this.session = _session;      
   this.errorHandler = _errorHandler;
-  this.language = _language;
-  this.fpc = null;    
+  this.stimulus = _stimulus;
+  this.fpc = null;
     
   this.audioFileLoader.addFile(this.stimulus.getFilepath(), (function (_buffer, _stimulus) { _stimulus.setAudioBuffer(_buffer); }), this.stimulus);
   this.filePlayer = null;
   this.waveformVisualizer = null;
-  this.likerts = [];
   this.ratingMap = {};
     
-  this.time = 0; 
+  this.time = 0;
   this.startTimeOnPage = null;
-  this.results= [];
 } 
 
-LikertSingleStimulusPage.prototype.getName = function () {
-  return this.pageConfig.name;
-};
-
-LikertSingleStimulusPage.prototype.init = function (_callbackError) { 
+LikertSingleStimulusPage.prototype.init = function (_callbackError) {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).init.call(this);
   this.filePlayer = new FilePlayer(this.audioContext, this.bufferSize, [this.stimulus], this.errorHandler, this.language, this.pageManager.getLocalizer(), this.pageConfig.showWaveform);
   
-  var cbk = (function(_prefix) {
-    this.ratingMap[_prefix] = true;
-    if (Object.keys(this.ratingMap).length == this.likerts.length) {
-      this.pageTemplateRenderer.unlockNextButton();
-    }
-  }).bind(this);
-  
-  
-  if (this.pageConfig.mustRate === false) {
-    cbk = false;  
-  }
-  
-  if (Array.isArray(this.pageConfig.response[0])) {
-    for (var i = 0; i < this.pageConfig.response.length; ++i) {
-      this.likerts.push(new LikertScale(this.pageConfig.response[i], i + '_', this.pageConfig.mustPlayback, cbk));
-    }
-  } else {
-    this.likerts.push(new LikertScale(this.pageConfig.response, '1_', this.pageConfig.mustPlayback, cbk));
-  }
- 
   if (this.pageConfig.mustPlayback) {
     this.filePlayer.genericAudioControl.addEventListener((function (_event) {
       if (_event.name == this.pageConfig.mustPlayback) {
-        this.likerts.map(function(l) {l.enable();});
+        for (i = 0; i < this.pageConfig.questionnaire.length; ++i) {
+          var element = this.pageConfig.questionnaire[i];
+          if (element.type === "likert") {
+            $('input[name='+element.name+'__response]').checkboxradio('enable');
+          }
+        }
       } 
     }).bind(this));
-
   }
-
 };
-  
-
-
 
 LikertSingleStimulusPage.prototype.render = function (_parent) {  
   var div = $("<div></div>");
@@ -93,42 +67,28 @@ LikertSingleStimulusPage.prototype.render = function (_parent) {
     this.waveformVisualizer.load();
   }
   this.filePlayer.render(_parent);
-
   
-  this.likerts.map(function(l) {l.render(_parent);});
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).render.call(this, _parent);
   
   this.fpc = new FilePlayerController(this.filePlayer);
   this.fpc.bind();
 };
 
-LikertSingleStimulusPage.prototype.load = function () {  
+LikertSingleStimulusPage.prototype.load = function () {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).load.call(this);
   this.startTimeOnPage = new Date();
-  if(this.pageConfig.mustRate == true){
-    this.pageTemplateRenderer.lockNextButton();
-  }
-  if(this.results.length > 0){
-    for (var i = 0; i < this.likerts.length; ++i) {
-      $("input[name='"+this.likerts[i].prefix +"_response'][value='"+this.results[i]+"']").attr("checked", "checked");
-      $("input[name='"+this.likerts[i].prefix +"_response'][value='"+this.results[i]+"']").checkboxradio("refresh");
-      this.likerts[i].group.change();
-    }
-  }
-  
   this.filePlayer.init();
 };
 
 LikertSingleStimulusPage.prototype.save = function () {
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).save.call(this);
   this.fpc.unbind(); 
   this.time += (new Date() - this.startTimeOnPage);
-  
-  for (var i = 0; i < this.likerts.length; ++i) {
-    this.results[i] = $("input[name='"+this.likerts[i].prefix +"_response']:checked").val();
-  }
   
   this.filePlayer.free();
 };
 
-LikertSingleStimulusPage.prototype.store = function (_reponsesStorage) {
+LikertSingleStimulusPage.prototype.store = function () {
   var trial = this.session.getTrial(this.pageConfig.type, this.pageConfig.id);
   if (trial === null) {
     trial = new Trial();
@@ -139,17 +99,8 @@ LikertSingleStimulusPage.prototype.store = function (_reponsesStorage) {
   var rating = new LikertSingleStimulusRating();
   rating.stimulus = this.stimulus.id;
   
-  rating.stimulusRating = [];
-  for(var i = 0; i < this.likerts.length; ++i){
-    if(this.results[i] === undefined){
-      rating.stimulusRating.push("NA");
-    }else{
-      rating.stimulusRating.push(this.results[i]);
-    }
-  }
-      
+  rating.stimulusRating = {};
+  Object.getPrototypeOf(LikertSingleStimulusPage.prototype).store.call(this, rating.stimulusRating);
   rating.time = this.time;
   trial.responses[trial.responses.length] = rating;
-
-
 };